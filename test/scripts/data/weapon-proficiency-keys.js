c5e00698baf5d037eb1b43a68f5ca9d428563335
pk991233 <61466408+pk991233@users.noreply.github.com>
2026-03-11 01:24:37 +0800

    Delete test directory

--- test/scripts/data/weapon-proficiency-keys.js
+++
@@ -1,4 +0,0 @@
-export const WEAPON_PROFICIENCY_KEY_MAP = {
-  sim: "TEST.WeaponProficiencies.sim",
-  mar: "TEST.WeaponProficiencies.mar"
-};
--- test/scripts/data/weapon-type-keys.js
+++
@@ -1,9 +0,0 @@
-export const WEAPON_TYPE_KEY_MAP = {
-  simpleM: "TEST.WeaponTypes.simpleM",
-  simpleR: "TEST.WeaponTypes.simpleR",
-  martialM: "TEST.WeaponTypes.martialM",
-  martialR: "TEST.WeaponTypes.martialR",
-  natural: "TEST.WeaponTypes.natural",
-  improv: "TEST.WeaponTypes.improv",
-  siege: "TEST.WeaponTypes.siege"
-};
